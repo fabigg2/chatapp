cd969feab6dfb3aa7b2271c5caed626f44a6d662
Fabian <fabig4da@gmail.com>
2022-02-10 21:51:06 -0500

    cors new host

--- src/infrastructure/settings/io/io.ts
+++ src/infrastructure/settings/io/io.ts
@@ -4,7 +4,7 @@ import { Server } from 'socket.io';
 export const socketIo = (app: any) => {
     return new Server(app, {
         cors: {
-            origin: "http://localhost:3000",
+            origin: ["http://localhost:3000","https://chatapp-fa.herokuapp.com" ],
             methods: ["GET", "POST"]
         }
     });
